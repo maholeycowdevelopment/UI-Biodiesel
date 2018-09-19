82c4b9a54a62d93860a6ceeafb469e18a90e70b3
Patrick VanVorce <maholeycowdevelopment@gmail.com>
2018-09-19 06:35:21 -0700

    created two stations folder directory

---
+++ biodiesel-lab/src/components/station1/ControlPanel.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ControlPanel extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="control-panel" />;
+  }
+}
+
+export default ControlPanel;
