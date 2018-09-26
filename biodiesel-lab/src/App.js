a7850a86fe4e847111db6f441d9f58938b6b4204
Patrick VanVorce <maholeycowdevelopment@gmail.com>
2018-09-26 09:59:10 -0700

    Passing inc station function as prop to children stations

--- biodiesel-lab/src/App.js
+++ biodiesel-lab/src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 import StationOne from "./components/StationOne";
+import StationTwo from "./components/StationTwo";
+import StationThree from "./components/StationThree";
+import StationFour from "./components/StationFour";
+import StationFive from "./components/StationFive";
+import StationSix from "./components/StationSix";
+import StationSeven from "./components/StationSeven";
 
 class App extends Component {
   constructor(props) {
@@ -20,10 +26,21 @@ class App extends Component {
   render() {
     switch (this.state.currentStation) {
       default:
-        return <StationOne />;
+        return <StationOne nextStation={this.nextStation.bind(this)} />;
       case 0:
-        return <StationOne />;
-        break;
+        return <StationOne nextStation={this.nextStation.bind(this)} />;
+      case 1:
+        return <StationTwo nextStation={this.nextStation.bind(this)} />;
+      case 2:
+        return <StationThree nextStation={this.nextStation.bind(this)} />;
+      case 3:
+        return <StationFour nextStation={this.nextStation.bind(this)} />;
+      case 4:
+        return <StationFive nextStation={this.nextStation.bind(this)} />;
+      case 5:
+        return <StationSix nextStation={this.nextStation.bind(this)} />;
+      case 6:
+        return <StationSeven nextStation={this.nextStation.bind(this)} />;
     }
   }
 }
